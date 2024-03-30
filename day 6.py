dc113af7ec0b4465374b7b69f5120849691b1e15
dishajain4689 <dishajain4689@gmail.com>
2024-03-30 19:07:24 +0530

    Init

---
+++ day 6.py
@@ -0,0 +1,20 @@
+a=1
+b=True
+c="disha"
+d=None
+a1=9
+print(a)
+print(b)
+print(a+a1)
+print("thetype of a is", type(a))
+print("the type of b is", type(b))
+print("the type of c is", type(c))
+print("the type of d is", type(d))
+list=[1,3,[5,10]]
+print(list)
+tuple=(("disha","rithu"),("disha","tanvi"))
+print(tuple)
+dict1={"name":"disha","age":20,"canvote":"yes"}
+print(dict1)
+
+

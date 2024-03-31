18b33a9eb4ebb1d981e974f6700630a7fc994d69
dishajain4689 <dishajain4689@gmail.com>
2024-03-31 13:01:22 +0530

    Init

---
+++ day789.py
@@ -0,0 +1,23 @@
+#addition
+print(5+8)
+#subtraction
+print(5-8)
+#multiplication
+print(5*8)
+#divisiov
+print(5/8)
+#floor division
+print(5//8)
+#exponential
+print(5**8)
+#modulus
+print(5%8)
+
+
+#how to make a calculator
+a=6
+b=2
+print("the value of" ,"a","+","b","is:a+b")
+print("the value of" ,"a","-","b","is:a-b")
+print("the value of" ,"a","*","b","is:a*b")
+print("the value of" ,"a","/","b","is:a/b")

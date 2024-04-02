0df9c810ecd7f9636a6ca3f85b751f3a394ee106
dishajain4689 <dishajain4689@gmail.com>
2024-04-02 22:07:25 +0530

    Init

--- day10.py
+++ day10.py
@@ -6,21 +6,21 @@ y= input("enter your second no:")
 #addition
 print(x+y)
 # ans= (if x=1, y=2) 12
-print(float(x) + float(y))
+print("sum=",float(x) + float(y))
 # ans = (if x=1, y=2) 3
 #subtraction
 x= input("enter your first no:")
 y= input("enter your second no:")
-print(float(x) - float(y))
+print("difference=",float(x) - float(y))
 # ans = (if x=1, y=2) -1
 #multipication
 x= input("enter your first no:")
 y= input("enter your second no:")
-print(float(x) * float(y))
+print("product=",float(x) * float(y))
 # ans = (if x=1, y=2) 2
 #division
 x= input("enter your first no:")
 y= input("enter your second no:")
-print(float(x) / float(y))
+print("quotent=",float(x) / float(y))
 # ans = (if x=1, y=2) 0.5
 

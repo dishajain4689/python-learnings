4e156fd309d88181504696be7c58714511dd7ade
dishajain4689 <dishajain4689@gmail.com>
2024-04-02 15:59:20 +0530

    Init

---
+++ day9.py
@@ -0,0 +1,17 @@
+#typecasting- the conversion of one data type into th other dat type
+a="1"
+b="2"
+print(a+b)
+# ans 12
+a=1
+b=2
+print(a+b)
+# ans 3
+a="1"
+b="2"
+print(int(a)+ int(b))
+# the above thing is explicit conversion (i am tell python, oii python convert (xyz)data type to (abc)data type. ex int to srting )
+#implicit typecasting(it converts a smaller data type to a higher data type to prevent data loss)
+c=1.9
+d=8
+print(c+d)

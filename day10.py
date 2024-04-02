3c367317daf44463ccd65e5307dbd65ca2ccbb21
dishajain4689 <dishajain4689@gmail.com>
2024-04-02 16:48:50 +0530

    Init

---
+++ day10.py
@@ -0,0 +1,26 @@
+# taking user input
+a=input("enter your name:")
+print("my name is", a)
+x= input("enter your first no:")
+y= input("enter your second no:")
+#addition
+print(x+y)
+# ans= (if x=1, y=2) 12
+print(int(x) + int(y))
+# ans = (if x=1, y=2) 3
+#subtraction
+x= input("enter your first no:")
+y= input("enter your second no:")
+print(int(x) - int(y))
+# ans = (if x=1, y=2) -1
+#multipication
+x= input("enter your first no:")
+y= input("enter your second no:")
+print(int(x) * int(y))
+# ans = (if x=1, y=2) 2
+#division
+x= input("enter your first no:")
+y= input("enter your second no:")
+print(int(x) / int(y))
+# ans = (if x=1, y=2) 0.5
+

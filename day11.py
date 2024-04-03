10eebc94fbee6d713063d0b020558445b01cf2df
dishajain4689 <dishajain4689@gmail.com>
2024-04-03 15:32:16 +0530

    Init

---
+++ day11.py
@@ -0,0 +1,18 @@
+a1="disha"
+a2="i love chocolates"
+a3="i am learning python"
+print(a1)
+print(a2)
+print(a3)
+print(a1[0])
+print(a1[1])
+print(a1[2])
+print(a1[3])
+print(a1[4])
+
+print("lets use a for loop\n")
+for character in a2:
+    print(character)
+print("\n")
+for character in a3:
+    print(character)

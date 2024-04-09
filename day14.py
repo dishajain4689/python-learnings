1c528df3d3e1b33ad0f194276f54a0aba661b55f
dishajain4689 <dishajain4689@gmail.com>
2024-04-09 21:35:08 +0530

    Init

---
+++ day14.py
@@ -0,0 +1,26 @@
+#if, else, elifin python
+#conditional operators 
+# <,>,<=,>=,==,!=(not equal to)
+a=int(input("enter your age: "))
+# print(a > 18)
+# print(a == 18)
+# print(a <= 18)
+print("your age is:",a)
+if(a>=18):
+ print("you can drive")
+else:
+ print("you cannot drive")
+ print("\n")
+b=int(input("enter the value of the no:"))
+if (b < 0) :
+  print("the no is negative")
+elif (b == 0):
+  print("the no is zer0")
+else:
+  print("the no is positive")
+
+
+
+
+
+ 
